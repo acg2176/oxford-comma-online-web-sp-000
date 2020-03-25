3d53036480147a86b871c97c82517230aa67ea89
Anna Guerrero <acg2176@columbia.edu>
2020-03-25 04:36:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,8 @@ if array.length() == 1
 elsif array.length() == 2
   return array.join(" and ")
 elsif array.length() == 3
-  first = array.slice(0,2)
-  first
+  array.insert(1, ",")
+  array.insert(3, ", and ")
 
 
 
