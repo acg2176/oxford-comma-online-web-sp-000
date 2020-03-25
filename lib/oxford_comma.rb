ca53aeea8e6ffd1bd2f1bad3f469abf20c9a9958
Anna Guerrero <acg2176@columbia.edu>
2020-03-25 04:21:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,11 +4,11 @@ if array.length() == 1
 elsif array.length() == 2
   return array.join(" and ")
 elsif array.length() == 3
-  last = array.pop
-  array << ","
-  second = array.pop
+  array.pop
+  array << ", and"
+  array.pop
   array<< ","
-  
+  array
 
 
 end
