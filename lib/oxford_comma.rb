cf30a5a1e7ad40ed412b2dde82b727be1a1e0400
Anna Guerrero <acg2176@columbia.edu>
2020-03-25 04:51:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,15 +8,6 @@ elsif array.length() == 3
   array.insert(3, ", and ")
   array.join
 elsif array.length > 3
-  #new_array = []
-  #new = array.pop
-  #array.each do |word|
-    #new_array.append(word)
-  #  new_array.append(", ")
-  #end
-  #new_array << "and "
-  #new_array << new
-  #new_array.join
   last = array.pop
   array.each do |word|
     word << ", "
